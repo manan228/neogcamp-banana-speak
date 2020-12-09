9e4d8ab49b3102abd98aa90053a836264463adb3
manan228 <68659116+manan228@users.noreply.github.com>
2020-12-09 20:57:01 +0530

    taking input ad console

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var btnTranslate = document.querySelector("#btnTranslate");
+var textInput = document.querySelector("#textInput");
+
+console.log(textInput)
 
 function callHandler() {
     console.log("Clicked");
+    console.log("input", textInput.value);
 }
 
 btnTranslate.addEventListener("click", callHandler)
