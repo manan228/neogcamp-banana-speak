dcbc5471021231c3180dc99a7124b128340ba8fa
manan228 <68659116+manan228@users.noreply.github.com>
2020-12-10 00:19:07 +0530

    Minions translator done js

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 var btnTranslate = document.querySelector("#btnTranslate");
 var textInput = document.querySelector("#textInput");
+var outputDiv = document.querySelector("#output");
 
-console.log(textInput)
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occurred", error)
+    alert("Something wrong with server! try again after some time")
+}
 
 function callHandler() {
-    console.log("Clicked");
-    console.log("input", textInput.value);
-}
+
+    var inputText = textInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+};
 
 btnTranslate.addEventListener("click", callHandler)
