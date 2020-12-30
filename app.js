6aaa37bb8cccd85f114f9f85e273e6224045e5de
manan228 <68659116+manan228@users.noreply.github.com>
2020-12-31 00:03:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var btnTranslate = document.querySelector("#btnTranslate");
 var textInput = document.querySelector("#textInput");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text) {
